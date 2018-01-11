8f1417a227fdf115eb5eb18d05fad9fd8b8626b8
Erik Hedenström <erik@brickchain.com>
2018-01-11 12:04:03 +0100

    Added headless tests to CI

--- karma.conf.js
+++ karma.conf.js
@@ -1,5 +1,6 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
+const isDocker = require('is-docker');
 
 module.exports = function (config) {
   config.set({
@@ -22,12 +23,18 @@ module.exports = function (config) {
     angularCli: {
       environment: 'dev'
     },
+    customLaunchers: {
+      ChromeCustom: {
+        base: 'ChromeHeadless',
+        flags: isDocker() ? ['--no-sandbox'] : []
+      }
+    },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['ChromeHeadless'],
     singleRun: false
   });
 };
