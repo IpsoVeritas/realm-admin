e06a6a734c59315040d6af05f8291f386e121ee0
Erik Hedenström <erik@brickchain.com>
2018-01-31 16:36:51 +0100

    Added role model and client

---
+++ src/app/shared/api-clients/roles.client.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { BaseClient } from './base.client';
+import { Role } from '../models';
+
+@Injectable()
+export class RolesClient extends BaseClient {
+
+  public getRoleIds(realmId: string): Promise<string[]> {
+    return this.config.getBackendURL(`/realms/${realmId}/roles`)
+      .then(url => this.http.get(url).toPromise())
+      .then(obj => <string[]>obj);
+  }
+
+  public getRole(realmId: string, roleId: string): Promise<Role> {
+    return this.config.getBackendURL(`/realms/${realmId}/roles/${roleId}`)
+      .then(url => this.http.get(url).toPromise())
+      .then(obj => this.jsonConvert.deserializeObject(obj, Role));
+  }
+
+  public getRoles(realmId: string): Promise<Role[]> {
+    return this.getRoleIds(realmId)
+      .then(roleIds => roleIds.map(roleId => this.getRole(realmId, roleId)))
+      .then(promises => Promise.all(promises));
+  }
+
+}
