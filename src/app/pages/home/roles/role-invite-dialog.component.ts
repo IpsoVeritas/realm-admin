56e434aa3c5f320cbfeca73ab24718946be122f9
Trân <tran@fruitdudragon.com>
2018-02-12 23:24:11 +0100

    add email validator in invite dialog

--- src/app/pages/home/roles/role-invite-dialog.component.ts
+++ src/app/pages/home/roles/role-invite-dialog.component.ts
@@ -1,23 +1,32 @@
 import { Component, Inject, HostListener } from '@angular/core';
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { FormControl, Validators } from '@angular/forms';
 import { Invite } from './../../../shared/models/';
 
 @Component({
   selector: 'app-role-invite-dialog',
   template: `
     <h2 mat-dialog-title>Invite member</h2>
-    <mat-dialog-content>
-      <mat-form-field>
-        <input matInput [(ngModel)]="invite.name" placeholder="Email" type="email">
-      </mat-form-field>
-      <mat-form-field>
-        <textarea matInput [(ngModel)]="invite.text" placeholder="Message"></textarea>
-      </mat-form-field>
-    </mat-dialog-content>
-    <mat-dialog-actions>
-      <button mat-raised-button [mat-dialog-close]="null">Cancel</button>
-      <button mat-raised-button [mat-dialog-close]="invite" color="accent">Send</button>
-    </mat-dialog-actions>`,
+    <form>
+      <mat-dialog-content>
+        <mat-form-field>
+          <input matInput [(ngModel)]="invite.name" name="email" placeholder="Email" type="email" [formControl]="emailFormControl" required>
+          <mat-error *ngIf="emailFormControl.hasError('email') && !emailFormControl.hasError('required')">
+            Please enter a valid email address
+          </mat-error>
+          <mat-error *ngIf="emailFormControl.hasError('required')">
+            Email is <strong>required</strong>
+          </mat-error>
+        </mat-form-field>
+        <mat-form-field>
+          <textarea matInput [(ngModel)]="invite.text" name="message" placeholder="Message" rows="4"></textarea>
+        </mat-form-field>
+      </mat-dialog-content>
+      <mat-dialog-actions>
+        <button mat-raised-button [mat-dialog-close]="null">Cancel</button>
+        <button mat-raised-button [mat-dialog-close]="invite" color="accent" [disabled]="(emailFormControl.hasError('email') || emailFormControl.hasError('required'))">Send</button>
+      </mat-dialog-actions>
+    </form>`,
   styles: [`
     mat-dialog-content {
       display: flex;
@@ -29,8 +38,13 @@ import { Invite } from './../../../shared/models/';
 })
 export class RoleInviteDialogComponent {
 
+  emailFormControl = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ]);
+
   constructor(public dialogRef: MatDialogRef<RoleInviteDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public invite: Invite) {
-  }
 
+  }
 }
