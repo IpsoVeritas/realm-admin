a4124b87031f388c5fb996e8205bf0b9286e7a5a
Erik Hedenström <erik@brickchain.com>
2018-05-30 18:03:21 +0200

    Working on realm popup

--- src/app/pages/login/realm-popup.component.ts
+++ src/app/pages/login/realm-popup.component.ts
@@ -1,16 +1,52 @@
 import { Component, ViewChild, OnInit, AfterViewInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { SessionService } from '../../shared/services';
+import { RealmsClient } from '../../shared/api-clients';
 
 @Component({
   selector: 'app-realm-popup',
   templateUrl: './realm-popup.component.html',
   styleUrls: ['./realm-popup.component.scss']
 })
-export class RealmPopupComponent {
-  title = 'Hello World';
+export class RealmPopupComponent implements OnInit {
 
-  constructor() {
-    setTimeout(_ => {
-      this.title = 'Updated!';
-    }, 5000);
+  realmForm: FormGroup;
+
+  constructor(private fb: FormBuilder,
+    private session: SessionService,
+    private realmsClient: RealmsClient) {
   }
+
+  ngOnInit() {
+    console.log(this.session.realm);
+    console.log(this.session.realms);
+    this.realmForm = this.fb.group({
+      'realm': [this.session.realm, Validators.required]
+    });
+    /*
+      this.route.paramMap.subscribe(paramMap => {
+
+        const realm = paramMap.get('realm');
+
+        this.realmForm = this.fb.group({
+          'realm': [realm, Validators.required]
+        });
+
+      });
+      */
+  }
+
+  get realm() {
+    return this.realmForm.get('realm');
+  }
+
+  onSubmit() {
+    return this.realmsClient.getRealmDescriptor(this.realm.value)
+      .then(descriptor => console.log(descriptor))
+      .catch(error => {
+        console.log(error);
+        this.realm.setErrors({ 'startAuthFailed': true });
+      });
+  }
+
 }
