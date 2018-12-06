ba84650765b39d391304020ef1ef4fa7bfdb5e90
Erik Hedenström <erik@brickchain.com>
2018-12-06 15:30:46 +0100

    Added new certificate model

---
+++ src/app/shared/models/v2/certificate.model.ts
@@ -0,0 +1,31 @@
+import { JsonObject, JsonProperty, Any } from 'json2typescript';
+import { DateConverter } from '../converters/date.converter';
+import { Base } from './base.model';
+
+@JsonObject
+export class Certificate extends Base {
+
+  constructor() {
+    super();
+    this.type = 'https://schema.brickchain.com/v2/certificate.json';
+  }
+
+  @JsonProperty('ttl', Number, true)
+  ttl: number = undefined;
+
+  @JsonProperty('issuer', Any, true)
+  issuer: any = undefined;
+
+  @JsonProperty('subject', Any, true)
+  subject: any = undefined;
+
+  @JsonProperty('documentTypes', [String], true)
+  documentTypes: string[] = undefined;
+
+  @JsonProperty('allowedRoles', [String], true)
+  allowedRoles: string[] = undefined;
+
+  @JsonProperty('keyLevel', Number, true)
+  keyLevel: number = undefined;
+
+}
